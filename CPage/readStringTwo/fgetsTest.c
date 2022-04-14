a1ceb0acd0cae6f138061410324c5e93d08a48bd
kingnan-Guo <373734199@qq.com>
2022-04-14 23:39:09 +0800

    更新代码文件夹结构

---
+++ CPage/readStringTwo/fgetsTest.c
@@ -0,0 +1,14 @@
+/**
+ * @file fgetsTest.c
+ * @author your name (you@domain.com)
+ * @brief 读取文件中的字符串
+ * @version 0.1
+ * @date 2022-04-14
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
