f2229c3600a882acd93abc1a379153d380f34dc0
kingnan <37373199@qq.com>
2022-04-24 12:54:21 +0800

    指针

---
+++ C++/point/point.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file point.cpp
+ * @author your name (you@domain.com)
+ * @brief 指针
+ * @version 0.1
+ * @date 2022-04-22
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+
+int main()
+{
+    int num1;
+    char string[10];
+    cout << "num1 =" << &num1 << endl;
+    cout << "string =" << &string << endl;
+    return 0;
+}
